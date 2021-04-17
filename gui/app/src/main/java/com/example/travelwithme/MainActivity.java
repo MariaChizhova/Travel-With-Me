ed3beaf9f9e97d39a8a6027fcccd0d1c86328d0e
shein <andrey.shein.spb@gmail.com>
2021-04-17 12:32:17 +0300

    update directory

---
+++ gui/app/src/main/java/com/example/travelwithme/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.travelwithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Firebase Authentication SDK
+    private FirebaseAuth mAuth;
+
+    // UI references
+    private BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // initialization of Firebase Authentication SDK
+        mAuth = FirebaseAuth.getInstance();
+
+        // check if user is signed in(not-null)
+        if (mAuth.getCurrentUser() == null) {
+            toLoginActivity();
+        }
+
+        // set SearchFragment for opening application
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new SearchFragment()).commit();
+
+        setContentView(R.layout.activity_main);
+
+        // initialization of UI references
+        bottomNavigation = findViewById(R.id.navigation_view);
+
+        // set function for the click
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationListener);
+    }
+
+    // Logic of bottom navigation
+    private final BottomNavigationView.OnNavigationItemSelectedListener navigationListener =
+            item -> {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.navigation_feed:
+                        selectedFragment = new FeedFragment();
+                        break;
+                    case R.id.navigation_messages:
+                        selectedFragment = new MessegesFragment();
+                        break;
+                    case R.id.navigation_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                    case R.id.navigation_settings:
+                        selectedFragment = new SettingsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                return true;
+            };
+
+    // move to the LoginActivity with sing out
+    private void toLoginActivity() {
+        mAuth.signOut();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+    }
+
+}
